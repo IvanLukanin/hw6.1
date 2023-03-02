4ec182260a4840cb3a6c61e58e9ed99e76e2dfaf
IVAN <luka.news.2001@mail.ru>
2023-03-02 15:20:54 +0300

    Домашнее задание: Строки - выполнено!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println("Ф.И.О сотрудника " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullname = "Ivanov Ivan Ivanovich";
+        String upperFullname = fullname.toUpperCase();
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperFullname);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullname = "Семёнов Семён Семёнович";
+        String replaceFullname = fullname.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + replaceFullname);
     }
 }
